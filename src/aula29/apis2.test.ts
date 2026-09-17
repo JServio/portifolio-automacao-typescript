a2b383f5ccf43201d3497e446fe13d9242439d9c
JServio <julieteservio.dev@gmail.com>
2026-09-17 14:45:42 +0000

    aula29

---
+++ src/aula29/apis2.test.ts
@@ -0,0 +1,69 @@
+import {test, expect} from 'vitest';
+
+const BASE_URL =  'https://jsonplaceholder.typicode.com';
+
+test('Metodo POST para criar um novo post', async () => {
+    const res = await fetch(`${BASE_URL}/posts`, {
+      method: 'POST',
+      headers:{
+        'Content-Type':'application/json'
+    },
+      body: JSON.stringify({
+        userId: 1,
+        title: 'Meu novo post',
+        body: 'Conteudo do meu novo post'
+    })
+});
+// Testa status code
+ expect(res.status).toBe(201);
+ // Testa se o returno é um objeto JSON
+ const dados = await res.json();
+ expect(dados.title).toBe('Meu novo post');
+ expect(dados.body).toBe( 'Conteudo do meu novo post');
+});
+
+
+test('Metodo PUT para criar ', async () => {
+    const res = await fetch(`${BASE_URL}/posts/1`, {
+      method: 'PUT',
+      headers:{
+        'Content-Type':'application/json'
+    },
+      body: JSON.stringify({
+        userId: 1,
+        title: 'MEU TITULO ATUALIZAR',
+        body: 'Conteudo do meu novo post'
+    })
+});
+// Testa status code
+ expect(res.status).toBe(200);
+ // Testa se o returno é um objeto JSON
+ const dados = await res.json();
+ expect(dados.title).toBe('MEU TITULO ATUALIZAR');
+ expect(dados.body).toBe( 'Conteudo do meu novo post');
+});
+
+test('Metodo PATCH para ATUALIZAR um post', async () => {
+    const res = await fetch(`${BASE_URL}/posts/1`, {
+      method: 'PATCH',
+      headers:{
+        'Content-Type':'application/json'
+    },
+      body: JSON.stringify({
+        title: 'MEU TITULO SUPER ATUALIZADO',
+    })
+});
+// Testa status code
+ expect(res.status).toBe(200);
+ // Testa se o returno é um objeto JSON
+ const dados = await res.json();
+ expect(dados.title).toBe('MEU TITULO SUPER ATUALIZADO');
+});
+
+test('Metodo DELETE para DELETAR um post',async () => {
+    const res = await fetch(`${BASE_URL}/posts/1`, {
+      method: 'DELETE',
+    });
+//Testa status code
+expect(res.status).toBe(200);
+});
